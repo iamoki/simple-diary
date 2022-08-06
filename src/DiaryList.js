8b3f2ca342b02f411f64b2cf183cd7e43625c2c4
iamoki <pureaoh@gmail.com>
2022-08-06 21:49:29 +0900

    ✅ 수정기능 구현

--- src/DiaryList.js
+++ src/DiaryList.js
@@ -1,6 +1,6 @@
 import DiaryItem from './DiaryItem';
 
-const DiaryList = ({ diaryList, onDelete }) => {
+const DiaryList = ({ diaryList, onRemove, onEdit }) => {
   return (
     <div className="DiaryList">
       <h2>일기리스트</h2>
@@ -8,7 +8,7 @@ const DiaryList = ({ diaryList, onDelete }) => {
       <div>
         {diaryList.map((it) => (
           // 데이터의 삭제나 생성시 발생할 인덱스값 변동으로 인해 일어난 오류를 피하기 위해 가급적 고유의 넘버를 지정해주자.
-          <DiaryItem key={it.id} {...it} onDelete={onDelete} />
+          <DiaryItem key={it.id} {...it} onRemove={onRemove} onEdit={onEdit} />
         ))}
       </div>
     </div>
