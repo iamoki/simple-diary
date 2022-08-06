4be1817dc9cd8a3a25b0521416aa177716b1e95e
iamoki <pureaoh@gmail.com>
2022-08-06 21:02:56 +0900

    ✅ 일기 삭제기능 추가

--- src/DiaryItem.js
+++ src/DiaryItem.js
@@ -1,4 +1,11 @@
-const DiaryItem = ({ author, content, created_date, emotion, id }) => {
+const DiaryItem = ({
+  author,
+  content,
+  created_date,
+  emotion,
+  id,
+  onDelete,
+}) => {
   return (
     <div className="DiaryItem">
       <div className="info">
@@ -11,7 +18,9 @@ const DiaryItem = ({ author, content, created_date, emotion, id }) => {
       <div className="content">{content}</div>
       <button
         onClick={() => {
-          console.log(id);
+          if (window.confirm(`${id}번째 일기를 정말 삭제하시겠습니까?`)) {
+            onDelete(id);
+          }
         }}
       >
         삭제하기
